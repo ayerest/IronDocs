495edbba41dc3f18669261c1b55c381ed94a6be5
ayerest <irisiseve@gmail.com>
2020-01-01 17:59:25 -0800

    switching to postgresql

--- db/migrate/20190821204535_add_alert_column_to_posts.rb
+++ db/migrate/20190821204535_add_alert_column_to_posts.rb
@@ -1,5 +1,5 @@
 class AddAlertColumnToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_column :posts, :alert_date, :date, null: true, default: :null
+    add_column :posts, :alert_date, :date
   end
 end
